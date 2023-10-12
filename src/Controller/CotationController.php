5480ff32675384032497a35dbc8305eef075af50
choquitofrito <zyriab@gmail.com>
2023-10-12 09:49:29 +0200

    correctio commentaires

--- src/Controller/CotationController.php
+++ src/Controller/CotationController.php
@@ -18,14 +18,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CotationController extends AbstractController
 {
 
+
     // exemple d'action pour afficher et traiter le form des cotations pour chaque critére
+    // Cette action sera appelée en cliquant le bouton "Donner une note"
     #[Route('/cotation/{id}', requirements: ['id' => '\d+'])] // requirements (id doit être numérique) pour que cette route ne se confonde pas avec /cotation/enregistrer plus bas
     public function formsCotations(
         Request $req,
         EntrepriseRepository $entRep,
         CritereRepository $criRep
     ): Response {
-        // il faut que cette action reçoit l'id de l'entreprise à côter
+        // il faut que cette action reçoit l'id de l'entreprise à côter 
         $id = $req->get('id');
         $entreprise = $entRep->find($id);
 
@@ -40,11 +42,11 @@ class CotationController extends AbstractController
         // UNE AUTRE ACTION les traitera car on utilisera AJAX et le submit de chaque form est indépendant
         $forms = [];
         foreach ($criteres as $critere) {
-            // Pour chaque Critere on stockera une cotation. L'user et l'entreprise sont déjà choisies maintenant
-            // il manque juste que l'User remplisse les données de la cotation
+            // Pour chaque Critere dans cette Entreprise on stockera une cotation. 
+            // L'user et l'entreprise sont déjà choisies quand on arrive à cette action alors en les fixe dans la Cotation 
+            // Maintenant il manque juste que l'User remplisse les données de la cotation
             $cotation = new Cotation();
             $cotation->setCritere($critere);
-
             $cotation->setEntreprise($entreprise);
             $cotation->setUser($user);
 
@@ -52,17 +54,14 @@ class CotationController extends AbstractController
             // on crée un form avec notre classe (CotationType) et on fixe l'action qui traite le form
             $form = $this->createForm(
                 CotationType::class,
-                $cotation,
-                ['attr' => [    // on rajouter des attributs... inspecter le DOM pour les voir
-                    'class' => 'form-cotation', // on aura besoin pour cibler le form en JS (voir script dans la vue)
-                    'data-critere' => $critere->getId() // on aura besoin quand on fera l'appel AXIOS (voir script dans la vue)
-                ]]
+                $cotation
+                
             );
 
 
             $forms[] = $form->createView(); // normallement on renvoie uniquement l'objet $form à la vue et 
-            // form_start lance createView, mais c'est uniquement possible 
-            // quand on renvoie un seul formulaire
+            // form_start lance createView en cachette, mais c'est uniquement possible 
+            // quand on renvoie un seul formulaire. Ici on doit envoyer l'array des formViews, pas juste les forms
         };
 
         return $this->render('cotation/forms_cotation.html.twig', [
@@ -71,6 +70,7 @@ class CotationController extends AbstractController
         ]);
     }
 
+    // cette action enregistre la cotation déjà complete. Elle ne renvoie pas une vue, sinon un JSON pour indiquer si tout ok ou pas
     #[Route('/cotation/enregistrer', name: 'enregistrer_cotation')]
     public function enregistrerCotation(
         Request $req,
@@ -78,12 +78,10 @@ class CotationController extends AbstractController
         ManagerRegistry $doctrine
     ) {
 
-
-
         // Récupérez l'entité entreprise en fonction de l'id du POST 
         $entrepriseId = $req->request->get('entreprise');
-        // dd($entrepriseId);
         $entreprise = $entRep->find($entrepriseId);
+        // dd($entrepriseId);
 
         // Récupérez le critére en fonction de l'id du POST
         $critereId = $req->request->get('critere');
@@ -98,18 +96,15 @@ class CotationController extends AbstractController
         $form->handleRequest($req);
 
         
-        // Parcourez les données du formulaire et enregistrez la cotation
         $entityManager = $doctrine->getManager();
         
         $critere = $doctrine->getRepository(Critere::class)->find($critereId);
+
         
-        // Créez et remplissez l'entité Cotation pour chaque critère
         $cotation->setEntreprise($entreprise);
         $cotation->setCritere($critere);
         $cotation->setUser($this->getUser());
         
-        // En supposant que 'note' est un champ de votre formulaire
-        // Remplissez les autres champs de manière similaire
         $entityManager->persist($cotation);
         //  dd($cotation);
 
